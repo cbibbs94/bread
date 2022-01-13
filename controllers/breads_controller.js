2b2a51b5731b8bb5fd5e857c4d7d747c307670a5
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-01-12 20:13:09 -0800

    initial Commit

---
+++ controllers/breads_controller.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/breads.js')
+
+// INDEX
+// INDEX
+breads.get('/', (req, res) => {
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+
+module.exports = breads
---
+++ views/layouts/default.jsx
@@ -0,0 +1,16 @@
+const React = require('react');
+
+function Default(html) {
+    return (
+      <html>
+      <head>
+        <title>{html.title || 'Default'}</title>
+      </head>
+      <body>
+        <h1>HTML Rendred!</h1>
+        {html.children}
+      </body>
+      </html>
+    )
+  }
+module.exports = Default
