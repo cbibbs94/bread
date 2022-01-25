f8a7b72b5ed44a317881fc310af357175fcf8537
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-01-24 21:16:16 -0800

    added server for data and redid code to match no params

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const req = require('express/lib/request')
 const {get} = require('express/lib/response')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -16,6 +17,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => { console.log('Oh Snap! We got that exclusive intel!! Its at:', process.env.MONGO_URI)})
 
 // ROUTES
 app.get('/', (req, res) => {
